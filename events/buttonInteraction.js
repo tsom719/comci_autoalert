fbbe4b959851e4541d629e2979bdea6b7bc80e0c
Seungjae Lee <lee@codesj.kr>
2023-05-26 23:44:26 +0900

    반 추가

--- events/buttonInteraction.js
+++ events/buttonInteraction.js
@@ -1,35 +1,13 @@
-/**
- * @file Button Interaction Handler
- * @author Naman Vrati
- * @since 3.0.0
- * @version 3.3.1
- */
-
 const { InteractionType, ComponentType } = require("discord-api-types/v10");
 
 module.exports = {
 	name: "interactionCreate",
 
-	/**
-	 * @description Executes when an interaction is created and handle it.
-	 * @author Naman Vrati
-	 * @param {import('discord.js').ButtonInteraction & { client: import('../typings').Client }} interaction The interaction which was created
-	 */
-
 	async execute(interaction) {
-		// Deconstructed client from interaction object.
 		const { client } = interaction;
-
-		// Checks if the interaction is a button interaction (to prevent weird bugs)
-
 		if (!interaction.isButton()) return;
 
 		const command = client.buttonCommands.get(interaction.customId);
-
-		// If the interaction is not a command in cache, return error message.
-		// You can modify the error message at ./messages/defaultButtonError.js file!
-
-		// A try to execute the interaction.
 		if (
 			interaction.customId.startsWith("1") ||
 			interaction.customId.startsWith("2") ||
