7be768145aa1976718d7d7d76f2173f754d829a6
Seungjae Lee <lee@codesj.kr>
2023-08-09 21:33:51 +0900

    주석 일부 삭제

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,16 +1,5 @@
-/**
- * @file Message Based Commands Handler
- * @author Naman Vrati
- * @since 1.0.0
- * @version 3.3.0
- */
-
-// Declares constants (destructured) to be used in this file.
-
 const { Collection, ChannelType } = require("discord.js");
 const { prefix, owner } = require("../config.json");
-
-// Prefix regex, we will use to match in mention prefix.
 
 const escapeRegex = (string) => {
 	return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
@@ -19,19 +8,8 @@ const escapeRegex = (string) => {
 module.exports = {
 	name: "messageCreate",
 
-	/**
-	 * @description Executes when a message is created and handle it.
-	 * @author Naman Vrati
-	 * @param {import('discord.js').Message & { client: import('../typings').Client }} message The message which was created.
-	 */
-
 	async execute(message) {
-		// Declares const to be used.
-
 		const { client, guild, channel, content, author } = message;
-
-		// Checks if the bot is mentioned in the message all alone and triggers onMention trigger.
-		// You can change the behavior as per your liking at ./messages/onMention.js
 
 		if (
 			message.content == `<@${client.user.id}>` ||
@@ -41,46 +19,19 @@ module.exports = {
 			return;
 		}
 
-		/**
-		 * @description Converts prefix to lowercase.
-		 * @type {String}
-		 */
-
 		const checkPrefix = prefix.toLowerCase();
-
-		/**
-		 * @description Regex expression for mention prefix
-		 */
 
 		const prefixRegex = new RegExp(
 			`^(<@!?${client.user.id}>|${escapeRegex(checkPrefix)})\\s*`
 		);
 
-		// Checks if message content in lower case starts with bot's mention.
-
 		if (!prefixRegex.test(content.toLowerCase())) return;
-
-		/**
-		 * @description Checks and returned matched prefix, either mention or prefix in config.
-		 */
 
 		const [matchedPrefix] = content.toLowerCase().match(prefixRegex);
 
-		/**
-		 * @type {String[]}
-		 * @description The Message Content of the received message seperated by spaces (' ') in an array, this excludes prefix and command/alias itself.
-		 */
-
 		const args = content.slice(matchedPrefix.length).trim().split(/ +/);
 
-		/**
-		 * @type {String}
-		 * @description Name of the command received from first argument of the args array.
-		 */
-
 		const commandName = args.shift().toLowerCase();
-
-		// Check if mesage does not starts with prefix, or message author is bot. If yes, return.
 
 		if (!message.content.startsWith(matchedPrefix) || message.author.bot)
 			return;
@@ -91,17 +42,11 @@ module.exports = {
 				(cmd) => cmd.aliases && cmd.aliases.includes(commandName)
 			);
 
-		// It it's not a command, return :)
-
 		if (!command) return;
-
-		// Owner Only Property, add in your command properties if true.
 
 		if (command.ownerOnly && message.author.id !== owner) {
 			return message.reply({ content: "This is a owner only command!" });
 		}
-
-		// Guild Only Property, add in your command properties if true.
 
 		if (command.guildOnly && message.channel.type === ChannelType.DM) {
 			return message.reply({
@@ -109,17 +54,12 @@ module.exports = {
 			});
 		}
 
-		// Author perms property
-		// Will skip the permission check if command channel is a DM. Use guildOnly for possible error prone commands!
-
 		if (command.permissions && message.channel.type !== ChannelType.DM) {
 			const authorPerms = message.channel.permissionsFor(message.author);
 			if (!authorPerms || !authorPerms.has(command.permissions)) {
 				return message.reply({ content: "You can not do this!" });
 			}
 		}
-
-		// Args missing
 
 		if (command.args && !args.length) {
 			let reply = `You didn't provide any arguments, ${message.author}!`;
